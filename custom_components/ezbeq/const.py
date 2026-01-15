3956bb484df3de7247188e7fb219d4077bd0f57d
j19801110 <roland.jutai@simplehomecinema.com>
2026-01-15 19:01:35 +1100

    Add new constants for sensor and switch entity IDs

--- custom_components/ezbeq/const.py
+++ custom_components/ezbeq/const.py
@@ -8,3 +8,16 @@ STATE_UNLOADED = "unloaded"
 # Sensor data
 CURRENT_PROFILE = "current_profile"
 DEVICES = "devices"
+
+# Manual-load entity IDs
+SENSOR_TMDB_IDS = "sensor.ezbeq_candidate_tmdb_ids"
+SENSOR_TITLES = "sensor.ezbeq_candidate_titles"
+SENSOR_DETAILS = "sensor.ezbeq_candidate_details"
+SENSOR_STATUS = "sensor.ezbeq_candidate_status"
+SWITCH_SEARCH_ENABLED = "switch.ezbeq_candidate_search_enabled"
+
+# Select entity ID (native SelectEntity)
+SELECT_CANDIDATE = "select.ezbeq_candidate"
+
+# Dispatcher signals
+SIGNAL_UPDATE_SELECT = "ezbeq_update_select"
